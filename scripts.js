27ba3f8fafa19418f594d912f94269479072a983
Wizhmazter <64862100+Wizhmazter@users.noreply.github.com>
2020-05-08 22:53:53 +0200

    More install startup

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let todaysDate = new Date();
 console.log(todaysDate);
-let dateElement = document.getElementById('date');
+let dateElement = document.getElementById("date");
 console.log(dateElement);
-dateElement.innerHTML = todaysDate.toISOString().substring(0,10);
+dateElement.innerHTML = todaysDate.toISOString().substring(0, 10);
